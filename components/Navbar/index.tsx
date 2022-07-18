02fe8611a6e3aa12e2870978cf8b7d5695e56462
Maksym Zinchenko <maklaut98@gmail.com>
2022-07-17 23:55:30 -0500

    Header menu added

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import * as Styled from "./navbar.styles";
+import HamburgerIcon from "./HamburgerIcon";
+import ProductTypes from "../ProductTypes";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <Styled.Navbar>
-      <Styled.Logo>audiophile</Styled.Logo>
-      <Styled.Cart>
-        <Styled.CartImg />
-      </Styled.Cart>
-    </Styled.Navbar>
-  )
+    <Styled.Header>
+      <Styled.HeaderPanel>
+        <Styled.MenuButton onClick={toggleMenu}>
+          <HamburgerIcon open={menuOpen} />
+        </Styled.MenuButton>
+        <Styled.Logo>audiophile</Styled.Logo>
+
+        <Styled.Cart>
+          <Styled.CartImg />
+        </Styled.Cart>
+      </Styled.HeaderPanel>
+      <Styled.Menu isOpen={menuOpen}>
+        <ProductTypes />
+      </Styled.Menu>
+    </Styled.Header>
+  );
 }
--- components/Navbar/navbar.styles.tsx
+++ components/Navbar/navbar.styles.tsx
@@ -1,26 +1,43 @@
-import styled from 'styled-components';
-export const Navbar = styled.nav`
-  background-color: ${props => props.theme.colors.backgroundBlack};
-  color: ${props => props.theme.colors.textGray};
-  height: 80px;
+import styled from "styled-components";
+interface menuStatus {
+  isOpen: boolean;
+}
+
+export const Header = styled.header`
+  background-color: ${(props) => props.theme.colors.backgroundBlack};
+  color: ${(props) => props.theme.colors.textGray};
   position: fixed;
   width: 100%;
-  border-bottom: 1px solid rgba(255,255,255, 0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   z-index: 1;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
-export const NavLink = styled.a`
-  
-`
+export const HeaderPanel = styled.section`
+  height: 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
+
 export const Logo = styled.a`
+  color: white;
+  font-size: 1.8rem;
+`;
+export const MenuButton = styled.div``;
 
-`
+export const Menu = styled.div<menuStatus>`
+  background-color: ${(props) => props.theme.colors.white};
+  display: ${(props) => (props.isOpen ? "block" : "none")};
+`;
+
 export const Cart = styled.a`
   width: 23px;
   height: 20px;
   cursor: pointer;
 `;
 
-export const CartImg = styled.img`
-
-`
+export const CartImg = styled.img``;
